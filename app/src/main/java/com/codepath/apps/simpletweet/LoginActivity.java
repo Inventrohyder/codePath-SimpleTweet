94d75251da20700c44dc6642acb87c83a1f1277d
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-10-10 06:32:22 +0200

    ✨ feat: Take user to TimelineActivity on successful login

--- app/src/main/java/com/codepath/apps/simpletweet/LoginActivity.java
+++ app/src/main/java/com/codepath/apps/simpletweet/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.simpletweet;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -47,8 +48,8 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	@Override
 	public void onLoginSuccess() {
 		Log.i(TAG, "onLoginSuccess: ");
-		// Intent i = new Intent(this, PhotosActivity.class);
-		// startActivity(i);
+		 Intent i = new Intent(this, TimelineActivity.class);
+		 startActivity(i);
 	}
 
 	// OAuth authentication flow failed, handle the error
