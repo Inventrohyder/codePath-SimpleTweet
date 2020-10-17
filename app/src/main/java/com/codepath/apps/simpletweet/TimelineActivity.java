5bdad63ee3a5a3509be13fd3b74a76bcfd3464b2
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-10-17 06:52:14 +0200

    ✨ feat: Setup ComposeActivity

--- app/src/main/java/com/codepath/apps/simpletweet/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/simpletweet/TimelineActivity.java
@@ -1,10 +1,10 @@
 package com.codepath.apps.simpletweet;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -139,8 +139,9 @@ public class TimelineActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.compose) {
             // Compose icon has been selected
-            Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
             // Navigate to compose activity
+            Intent intent = new Intent(this, ComposeActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
